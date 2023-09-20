6cf0148dff56263d02f53d01c6e8b578acb64e39
Izobov <izobov9@gmail.com>
2023-09-20 09:47:12 +0300

    Functions

---
+++ contracts/Functions.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Functions {
+    string message = "Hello";
+
+    //public   visible anyware
+    //external visible outside contract but not in contract
+    //internal visible in contract + childs
+    //private visible only in contract
+
+    //view read blockchain vars
+    //pure 
+
+    function getBalance() public view returns(uint) {
+        return  address(this).balance;
+    }
+
+    function getBalance2() public view returns(uint balance) {
+        balance = address(this).balance;
+        return  balance;
+    }
+
+    function setMessage(string memory newMsg) external {
+        message = newMsg;
+    }
+
+    function getMessage() external view returns(string memory) {
+       return  message;
+    }
+
+
+}
