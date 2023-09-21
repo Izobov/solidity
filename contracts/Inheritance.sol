fb179c04121007c859ab66771012db6d258156e2
Izobov <izobov9@gmail.com>
2023-09-21 18:59:53 +0300

    Inheritance

---
+++ contracts/Inheritance.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Ownable {
+address public owner;
+
+    constructor(address ownerOverride) {
+        owner = ownerOverride == address(0) ? msg.sender : ownerOverride;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "not an owner!");
+        _;
+    }
+
+    
+    function withdraw(address payable _to) public virtual  onlyOwner {
+        payable (owner).transfer(address(this).balance);
+    }
+}
+
+abstract contract Balances is Ownable {
+    function getBalance() public view onlyOwner returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdraw(address payable _to) public override virtual onlyOwner {
+        _to.transfer(getBalance());
+        
+    }
+
+}
+
+contract MyContract is Ownable, Balances {
+    constructor(address _owner) Ownable(_owner) {
+    }
+    // constructor(address _owner) {
+    //     owner = _owner;
+    // }
+    function withdraw(address payable _to) public override(Ownable, Balances) {
+        // Balances.withdraw(_to);
+        super.withdraw(_to);
+    }
+  
+}
